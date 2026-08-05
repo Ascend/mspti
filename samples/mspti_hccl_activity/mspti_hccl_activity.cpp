4ebb24f25051b4d1f7903ed67e3e520ca5897633
eejiechu <wangjie659@huawei.com>
2026-08-05 17:19:16 +0800

    mspti hccl activity sample fix
    Co-authored-by: eejiechu<wangjie659@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !158 merge mspti_hccl_sample_fix into master
    
    mspti hccl activity sample fix
    
    Created-by: eejiechu
    Commit-by: eejiechu
    Merged-by: ascend-robot
    Description: # PR 合入模板
    
    **注：经过自检不涉及的可标注“不涉及”或直接打勾，特殊情况请文字备注。不符合规范的 PR 不允许合入，请（后备）commit 注意。**
    
    ----
    
    ## 1. 修改描述
    
    - **修改原因：**  mspti hccl activity 样例代码修复
    - **修改内容：**  
    1. 修复 Hccl 通信域销毁时的device清理流程缺失：在 main 函数末尾 HcclCommDestroy 调用前新增 ACL_CALL(aclrtSetDevice(devices[i]))，调用后新增 ACL_CALL(aclrtResetDevice(devices[i]))，确保每个hcclcomm在正确的设备上销毁并重置Device
    2. pre-commit相关
    ----
    
    ## 2. 功能验证
    
    - [x] **功能自验**
    ci pre-smoke 会跑hccl activity的样例，正常通过
    ![image.png](https://raw.gitcode.com/user-images/assets/8528170/135bfc90-c7a3-432b-a133-748c7ec13451/image.png 'image.png')
    ![image.png](https://raw.gitcode.com/user-images/assets/8528170/b5c836c7-4916-40f2-b2b9-0e376851a6c1/image.png 'image.png')
    - [ ] **本地自验用例截图**
    - [ ] **冒烟是否通过** （填入群链接的自验证报告中，如未通过，请说明原因：____________________ ，功能代码请主动申报添加冒烟）
    
    ----
    
    ## 3. 分支合并要求
    
    - [ ] **代码合并**（请确保将 master 分支的最新代码同步合并至 poc 分支及 pre-research 分支，同时保证 poc 分支的代码也已正确合并到 pre-research 分支。）
    
    ----
    
    ## 4. 代码检视
    
    - **要求：**
      - 合入代码超过 200 行，需三人以上会议检视。
      - 检视密度≥1个/100行。
      - 检视缺陷密度未达要求需提供说明。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**
    - [ ] **是否具备 UT 测试用例看护** （如不符合，请说明原因：____________________）
    
    - **检视意见数：____ 条** （请填写本次检视的意见总数，用于commit合入前审视）
    
    ----
    
    ## 5. 安全自检
    
    ### Python、C++
    
    - [ ] **对外接口新增/删除/变更后，资料要同步新增/删除/变更，新增接口入参校验参考外部输入表格**
    - [ ] **不允许私有的文件操作，需要使用公共模块的安全函数**
    - [ ] **任务结束后需要删除临时文件，同时需要考虑任务失败后，临时文件没有残留**
    - [ ] **数组访问需要校验越界场景，对除法需要做除零校验**
    - [ ] **需要对递归方法做递归深度校验，正则表达式必须做 ReDoS 校验**
    - [ ] **需要充分进行接口输入和返回值异常情况的校验**
    - [ ] **日志打印不要出现拼写或语法错误，不要暴露代码细节和敏感信息**
    
    ### C++
    
    - [ ] **指针使用前需要判空**
    - [ ] **数值计算校验溢出和反转**
    - [ ] **不可存在内存泄漏（异常场景需要释放内存）**
    - [ ] **类型转换不能出现数据截断**
    - [ ] **拷贝字符串时，目的缓冲区至少比源缓冲区大 1**
    - [ ] **拷贝内存时，目的缓冲区不小于源缓冲区**
    - [ ] **内存释放后指针赋值为 nullptr**
    
    ----
    
    ## 6. 变更知会
    
    - [ ] **资料修改**
    - [ ] **变更通知（消息知会 + 邮件知会）**
    
    ----
    
    
    See merge request: Ascend/mspti!158

--- samples/mspti_hccl_activity/mspti_hccl_activity.cpp
+++ samples/mspti_hccl_activity/mspti_hccl_activity.cpp
@@ -13,14 +13,13 @@
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  * -------------------------------------------------------------------------
-*/
+ */
 
 // System headers
 #include <cstring>
-#include <vector>
 #include <memory>
 #include <thread>
-#include <unistd.h>
+#include <vector>
 
 // Acl header
 #include "acl/acl.h"
@@ -31,18 +30,19 @@
 #include "hccl/hccl_types.h"
 
 // Mspti header
-#include "mspti/mspti.h"
 #include "common/helper_mspti.h"
 #include "common/util_acl.h"
+#include "mspti/mspti.h"
 
-struct ThreadContext {
+struct ThreadContext
+{
     HcclComm comm;
     int32_t device;
 };
 
-int HcclSample(void *arg)
+int HcclSample(void* arg)
 {
-    ThreadContext* ctx = (ThreadContext *)arg;
+    ThreadContext* ctx = (ThreadContext*)arg;
     ACL_CALL(aclrtSetDevice(ctx->device));
     aclrtStream stream;
     ACL_CALL(aclrtCreateStream(&stream));
@@ -54,24 +54,27 @@ int HcclSample(void *arg)
     int mallocSize = count * sizeof(float);
     ACL_CALL(aclrtMallocHost(static_cast<void**>(&hostBuff), mallocSize));
     float* tmpHostBuff = static_cast<float*>(hostBuff);
-    for (uint32_t i = 0; i < count; ++i) {
+    for (uint32_t i = 0; i < count; ++i)
+    {
         tmpHostBuff[i] = i;
     }
     ACL_CALL(aclrtMalloc((void**)&sendBuff, mallocSize, ACL_MEM_MALLOC_HUGE_FIRST));
     ACL_CALL(aclrtMemcpy((void*)sendBuff, mallocSize, (void*)hostBuff, mallocSize, ACL_MEMCPY_HOST_TO_DEVICE));
     ACL_CALL(aclrtMalloc((void**)&recvBuff, mallocSize, ACL_MEM_MALLOC_HUGE_FIRST));
 
-    HCCL_CHECK(HcclAllReduce((void *)sendBuff, (void*)recvBuff, count, HCCL_DATA_TYPE_FP32, HCCL_REDUCE_SUM, ctx->comm,
+    HCCL_CHECK(HcclAllReduce((void*)sendBuff, (void*)recvBuff, count, HCCL_DATA_TYPE_FP32, HCCL_REDUCE_SUM, ctx->comm,
                              stream));
     ACL_CALL(aclrtSynchronizeStream(stream));
 
     // 8为单机device卡数
-    if (ctx->device < 8) {
+    if (ctx->device < 8)
+    {
         void* resultBuff;
         ACL_CALL(aclrtMallocHost(static_cast<void**>(&resultBuff), mallocSize));
         ACL_CALL(aclrtMemcpy((void*)resultBuff, mallocSize, (void*)recvBuff, mallocSize, ACL_MEMCPY_DEVICE_TO_HOST));
         float* tmpResBuff = static_cast<float*>(resultBuff);
-        for (uint32_t i = 0; i < count; ++i) {
+        for (uint32_t i = 0; i < count; ++i)
+        {
             LOG_PRINT("rankId: %u, i: %f\n", ctx->device, tmpResBuff[i]);
         }
         ACL_CALL(aclrtFreeHost(resultBuff));
@@ -91,7 +94,7 @@ void SetUpMspti()
     msptiActivityEnable(MSPTI_ACTIVITY_KIND_HCCL);
 }
 
-int main(int argc, char*argv[])
+int main(int argc, char* argv[])
 {
     SetUpMspti();
 
@@ -99,30 +102,36 @@ int main(int argc, char*argv[])
     uint32_t ndev = 8;
     int32_t devices[8] = {0, 1, 2, 3, 4, 5, 6, 7};
     HcclComm comms[ndev];
-    for (uint32_t i = 0; i < ndev; i++) {
+    for (uint32_t i = 0; i < ndev; i++)
+    {
         ACL_CALL(aclrtSetDevice(devices[i]));
     }
     HCCL_CHECK(HcclCommInitAll(ndev, devices, comms));
     std::vector<std::unique_ptr<std::thread>> threads(ndev);
     struct ThreadContext args[ndev];
-    for (uint32_t i = 0; i < ndev; i++) {
+    for (uint32_t i = 0; i < ndev; i++)
+    {
         args[i].device = i;
         args[i].comm = comms[i];
-        threads[i].reset(new (std::nothrow) std::thread(&HcclSample, (void *)&args[i]));
-        std::chrono::seconds duration(6); // 设置6秒睡眠，保证线程正常运行
+        threads[i].reset(new (std::nothrow) std::thread(&HcclSample, (void*)&args[i]));
+        std::chrono::seconds duration(6);  // 设置6秒睡眠，保证线程正常运行
         std::this_thread::sleep_for(duration);
     }
-    for (uint32_t i = 0; i < ndev; i++) {
-        if (threads[i] && threads[i]->joinable()) {
+    for (uint32_t i = 0; i < ndev; i++)
+    {
+        if (threads[i] && threads[i]->joinable())
+        {
             threads[i]->join();
         }
     }
-    for (uint32_t i = 0; i < ndev; i++) {
+    for (uint32_t i = 0; i < ndev; i++)
+    {
+        ACL_CALL(aclrtSetDevice(devices[i]));
         HCCL_CHECK(HcclCommDestroy(comms[i]));
+        ACL_CALL(aclrtResetDevice(devices[i]));
     }
     ACL_CALL(aclFinalize());
 
     DeInitMspti();
     return 0;
 }
-
